1c931f5826dd77614275dfaea72f8324ffc759b2
Davialcantarati <davialcantara.ti@gmail.com>
2020-07-14 23:03:31 -0300

    Set snake movements

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 };
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "Lightgreen";
@@ -19,5 +20,29 @@ function criarCobra(){
     }
 };
 
+function startGame(){
 criarBG();
 criarCobra();
+
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    
+    snake.unshift(newHead);
+
+}
+
+
+
+let jogo = setInterval(startGame, 500);
